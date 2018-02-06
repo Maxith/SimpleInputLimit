3d0132f49a157034c15b444c785b1f3e6949cdf9
Maxith zhou <zhouyou457@hotmail.com>
2018-02-06 17:12:38 +0800

    全新版本~~

--- src/inputLimit.js
+++ src/inputLimit.js
@@ -1,144 +1,181 @@
 /**
- * 限制输入框 v0.1beta
+ * 限制输入框 v0.2
  * 基于jquery简单实现文本框输入限制
+ * 
+ * 	我系渣渣辉,介系里没有挽过的船新版本,挤需体验三番钟，里造会汗我一样，爱象借款插件
+ * 
  * @auther Maxith chou
+ * @date 2018年2月6日
  */
-$(document).ready(function(){bindInput();});
-function bindInput(){
-	var all = document.all ? document.all : $('input[type="text"]'),doms = new Array();
-	for (var i = 0; i < all.length; i++) {
-		var type = $(all[i]).attr('inputLimit');
-		if(type != null){
-			doms[doms.length] = all[i];
-		}
-	}
-	for (var i = 0; i < doms.length; i++) {
-		var item = doms[i],type = $(item).attr('inputLimit');
-		switch (type){
-			case 'number_only':
-				item.onkeypress = function(event){
-					var e = window.event||event,
-						code = e.keyCode || e.which;
-					return code>=48&&code<=57;
-				};
-				item.onblur = function(event){
-					if(this.value.lastIndexOf(".")==(this.value.length-1)){
-		                this.value = this.value.substr(0,this.value.length-1);
-	                }else if(isNaN(this.value)){
-	                    this.value = "";
-	                }
-				};
-				item.onpaste = function(event){
-					var s=clipboardData.getData('text');
-	                if(!/\D/.test(s));
-	                value=s.replace(/^0*/,'');
-	                return false;
-				};
-				item.ondragenter = function(event){
-					return false;
-				};
-				item.onkeyup = function(event){
-					if(this.value.length >= 2){
-						if(/(^0+)/.test(this.value))this.value=this.value.replace(/^0*/, '');
-					}
-				};
-				break;
-			case 'number_decimal':
-				item.onkeypress = function(event){
-					var e = window.event||event,
-						code = e.keyCode || e.which;
-					if(code == 46){
-						if(this.value.length < 1){
-							return false;
-						}
-	                    if(this.value.indexOf(".")!= -1){
-	                        return false;
-	                    }
-	                }else{
-	                    return code == 46||code>=48&&code<=57;
-	                }
-				};
-				item.onblur = function(event){
-					if(this.value.lastIndexOf(".")==(this.value.length-1)){
-		                this.value = this.value.substr(0,this.value.length-1);
-	                }else if(isNaN(this.value)){
-	                    this.value = "";
-	                }
-				};
-				item.onpaste = function(event){
-					var s=clipboardData.getData('text');
-	                if(!/\D/.test(s));
-	                value=s.replace(/^0*/,'');
-	                return false;
-				};
-				item.ondragenter = function(event){
-					return false;
-				};
-				item.onkeyup = function(event){
-					if(this.value.indexOf(".") == -1){
-						if(this.value.length >= 2){
-							if(/(^0+)/.test(this.value))this.value=this.value.replace(/^0*/, '');
-						}
-					}else{
-						if(this.value.length == 1){
-							this.value = '';
-						}
-					}
-				};
-				break;
-			case 'number_integer':
-				item.onkeypress = function(event){
-					var e = window.event||event,
-						code = e.keyCode || e.which;
-					if(code == 45){
-	                    if(getCursorPosition(this) > 0){
-	                    	return false;
-	                    }
-						if(this.value.indexOf("-")!=-1){
-	                        return false;
-	                    }
-	                }
-					return code == 45||code>=48&&code<=57;
-				};
-				item.onblur = function(event){
-					if(this.value.lastIndexOf(".")==(this.value.length-1)){
-		                this.value = this.value.substr(0,this.value.length-1);
-	                }else if(isNaN(this.value)){
-	                    this.value = "";
-	                }
-				};
-				item.onpaste = function(event){
-					var s=clipboardData.getData('text');
-	                if(!/\D/.test(s));
-	                value=s.replace(/^0*/,'');
-	                return false;
-				};
-				item.ondragenter = function(event){
-					return false;
-				};
-				item.onkeyup = function(event){
-					if(this.value.length >= 2){
-						if(/(^0+)/.test(this.value))this.value=this.value.replace(/^0*/, '');
-					}
-				};
-				break;
-				//TODO 其余类型限制
-		}
-	}
-}
+//正整数
+$(document).on({
+    keypress: function (event) {
+        var e = window.event || event,
+            code = e.keyCode || e.which;
+        if (code == 46) {
+            if (this.value.length < 1) {
+                return false;
+            }
+            if (this.value.indexOf(".") != -1) {
+                return false;
+            }
+        } else {
+            if (this.value.length != 0 && this.value.indexOf(".") != -1
+                && (this.value.length - this.value.indexOf(".")) > 2
+                && (getCursorPosition(this) - this.value.indexOf(".")) > 0) {
+                return false;
+            }
+            return code == 46 || code >= 48 && code <= 57;
+        }
+    },
+    blur: function (event) {
+        if (this.value.lastIndexOf(".") == 0) {
+            this.value = '0' + this.value;
+        }
+        if (this.value.lastIndexOf(".") == (this.value.length - 1)) {
+            this.value = this.value.substr(0, this.value.length - 1);
+        } else if (isNaN(this.value)) {
+            this.value = "";
+        }
+    },
+    paste: function (event) {
+        var s = event.originalEvent.clipboardData.getData('text');
+        if (!/\D/.test(s)) ;
+        this.value = s.replace(/^0*/, '');
+        return false;
+    },
+    dragenter: function (event) {
+        return false;
+    },
+    keyup: function (event) {
+        if (this.value.indexOf(".") == -1) {
+            if (this.value.length >= 2) {
+                if (/(^0+)/.test(this.value)) this.value = this.value.replace(/^0*/, '');
+            }
+        } else {
+            if (this.value.length == 1) {
+                this.value = '';
+            }
+        }
+    }
+}, '.input-limit-positive');
+
+//负整数
+$(document).on({
+    keypress: function (event) {
+        var e = window.event || event,
+            code = e.keyCode || e.which;
+        if (code == 46) {
+            if (this.value.length < 1) {
+                return false;
+            }
+            if (this.value.indexOf(".") != -1) {
+                return false;
+            }
+        } else if (code == 45) {
+            if (getCursorPosition(this) > 0) {
+                return false;
+            }
+            if (this.value.indexOf("-") != -1) {
+                return false;
+            }
+        } else {
+            if (this.value.length != 0 && this.value.indexOf(".") != -1
+                && (this.value.length - this.value.indexOf(".")) > 2
+                && (getCursorPosition(this) - this.value.indexOf(".")) > 0) {
+                return false;
+            }
+            return code == 45 || code == 46 || code >= 48 && code <= 57;
+        }
+    },
+    blur: function (event) {
+        if (this.value.lastIndexOf(".") == 0) {
+            this.value = '0' + this.value;
+        }
+        if (this.value.lastIndexOf(".") == (this.value.length - 1)) {
+            this.value = this.value.substr(0, this.value.length - 1);
+        } else if (isNaN(this.value)) {
+            this.value = "";
+        }
+    },
+    paste: function (event) {
+        var s = event.originalEvent.clipboardData.getData('text');
+        if (!/\D/.test(s)) ;
+        this.value = s.replace(/^0*/, '');
+        return false;
+    },
+    dragenter: function (event) {
+        return false;
+    },
+    keyup: function (event) {
+        if (this.value.indexOf(".") == -1) {
+            if (this.value.length >= 2) {
+                if (/(^0+)/.test(this.value)) this.value = this.value.replace(/^0*/, '');
+            }
+        } else {
+            if (this.value.length == 1) {
+                this.value = '';
+            }
+        }
+    }
+}, '.input-limit-minus');
+
+$(document).on({
+    keypress: function (event) {
+        var e = window.event || event,
+            code = e.keyCode || e.which;
+        if (code == 46) {
+            if (this.value.length < 1) {
+                return false;
+            }
+            if (this.value.indexOf(".") != -1) {
+                return false;
+            }
+        } else {
+            return code == 46 || code >= 48 && code <= 57;
+        }
+    },
+    blur: function (event) {
+        if (this.value.lastIndexOf(".") == (this.value.length - 1)) {
+            this.value = this.value.substr(0, this.value.length - 1);
+        } else if (isNaN(this.value)) {
+            this.value = "";
+        }
+    },
+    paste: function (event) {
+        var s = event.originalEvent.event.originalEvent.clipboardData.getData('text');
+        if (!/\D/.test(s)) ;
+        this.value = s.replace(/^0*/, '');
+        return false;
+    },
+    dragenter: function (event) {
+        return false;
+    },
+    keyup: function (event) {
+        if (this.value.indexOf(".") == -1) {
+            if (this.value.length >= 2) {
+                if (/(^0+)/.test(this.value)) this.value = this.value.replace(/^0*/, '');
+            }
+        } else {
+            if (this.value.length == 1) {
+                this.value = '';
+            }
+        }
+    }
+}, 'input-limit-decimal');
 /**
  * 光标位置判断
  */
 function getCursorPosition(el){
-     var pos = 0;
-     if ('selectionStart' in el) {
-         pos = el.selectionStart;
-     } else if ('selection' in document) {
-         el.focus();
-         var Sel = document.selection.createRange();
-         var SelLength = document.selection.createRange().text.length;
-         Sel.moveStart('character', -el.value.length);
-         pos = Sel.text.length - SelLength;
-     }
-     return pos;
- }
+    var pos = 0;
+    if ('selectionStart' in el) {
+        pos = el.selectionStart;
+    } else if ('selection' in document) {
+        el.focus();
+        var Sel = document.selection.createRange();
+        var SelLength = document.selection.createRange().text.length;
+        Sel.moveStart('character', -el.value.length);
+        pos = Sel.text.length - SelLength;
+    }
+    return pos;
+}
